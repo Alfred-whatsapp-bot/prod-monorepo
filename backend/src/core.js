e7a468fbc13383782d4a669a7b095a2274db4ba0
Luis <1luisfelipe2@gmail.com>
2023-01-28 17:17:47 -0400

    ajuste commit anterior

--- backend/src/core.js
+++ backend/src/core.js
@@ -181,7 +181,7 @@ export async function httpCtrl(name, port) {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   app.use(express.static(path.join(__dirname, "dist")));
-  app.use("/", function (req, res) {
+  app.use("/index", function (req, res) {
     //res.sendFile(path.join(__dirname, "dist/frontend/index.html"));
     const buffer = fs.readFileSync(path.join(__dirname, "dist/index.html"));
     let html = buffer.toString();
