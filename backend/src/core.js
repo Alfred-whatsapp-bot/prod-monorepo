96b08a757d3080bd13d4d6d21bdeb74026e5af3a
Luis <1luisfelipe2@gmail.com>
2023-02-05 02:02:44 -0400

    .

--- backend/src/core.js
+++ backend/src/core.js
@@ -26,7 +26,7 @@ export async function httpCtrl(name, port) {
   const app = express();
   app.use(cors());
   app.use(bodyParser.json()); // support json encoded bodies
-  app.use(forceSSL);
+  // app.use(forceSSL);
   //app.enable("trust proxy");
   // if (!fs.existsSync("logs")) {
   //   fs.mkdirSync("logs", { recursive: true });
