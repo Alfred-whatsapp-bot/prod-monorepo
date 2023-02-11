acda2fd71fe98b0c793fb338337eefb20b201245
Luis <1luisfelipe2@gmail.com>
2023-02-11 16:30:35 -0400

    .

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   logged: false,
-  urlApi: "http://89.117.32.179:3001",
+  //urlApi: "http://89.117.32.179:3001",
+  urlApi: "http://alfredbot.ignorelist.com:3001",
 };
