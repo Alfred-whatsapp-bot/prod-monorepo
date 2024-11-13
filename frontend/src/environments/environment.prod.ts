0263c72d86c361cd965d31da90d731d8f88da6d5
Luis Felipe <1luisfelipe2@gmail.com>
2024-11-12 20:28:21 -0400

    Atualiza a URL da API para o ambiente de produção

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   logged: false,
   //urlApi: "http://89.117.32.179:3001",
-  urlApi: "https://portal-nda-api.onrender.com",
+  urlApi: "https://prod-monorepo.onrender.com",
 };
